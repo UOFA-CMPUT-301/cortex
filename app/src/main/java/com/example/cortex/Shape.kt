ceb17c6d1a710017ee4e58cb951fb823ae22e95f
JACK <jacksen@ualberta.ca>
2026-09-24 18:38:46 -0600

    Added function

--- app/src/main/java/com/example/cortex/Shape.kt
+++ app/src/main/java/com/example/cortex/Shape.kt
@@ -1,6 +1,18 @@
 package com.example.cortex
 
+import androidx.compose.remote.creation.compose.state.pow
+
 abstract class Shape (
     var x: Int,
     var y: Int
 )
+
+class Hexagon(posx: Int, posy: Int, sideLen: Double): Shape(posx,posy) {
+    fun area(): Double {
+        return 3*(3.pow(1/2))/2 * sideLen.pow(2)
+    }
+
+    fun perimeter(): Double {
+        return 6*sideLen
+    }
+}
